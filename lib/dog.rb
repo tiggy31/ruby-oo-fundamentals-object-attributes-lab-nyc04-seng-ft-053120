589f08f7dec20c728ba7e5ab31323ca8cbd262e3
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-23 02:56:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,46 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+
+class Person
+  
+  def name
+    @name
+  end
+  
+  
+  def name=(person_name)
+    @name = person_name
+end
+end
+
+
+
+
+class Person
+  
+  def job
+    @job
+  end
+  
+  def job=(person_job)
+    @job= person_job
+  end
+end
+
